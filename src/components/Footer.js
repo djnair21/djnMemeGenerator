edff4943001d474e31bb8fb3653936c45e61c4e2
Dhananjay Nair <djnair21@gmail.com>
2022-07-12 23:46:59 +0530

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+       <footer className="footer">
+        <p> created with love, by DJN</p>
+        </footer> 
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className="header">
+      <img src= '/images/abc.png' alt = "abc" className= "header-img"/>
+        <h2 className= "header-t1">Random Memes</h2>
+        <h4  className= "header-t2"> Djn Meme Generator</h4>
+
+
+    </header>
+  )
+}
+
+export default Header
